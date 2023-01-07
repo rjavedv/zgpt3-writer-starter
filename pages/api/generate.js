58aa3f67acecd63a5be253067e6a3b3b96054bfb
rjavedv <34070318+rjavedv@users.noreply.github.com>
2023-01-07 19:02:58 +0300

    Working for 3 inputs

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,24 +1,36 @@
 import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
-  organization: "org-iVQ41YmUXddf15AGKMhfSqQJ",  
+  organization: "org-iVQ41YmUXddf15AGKMhfSqQJ",
   apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
-const basePromptPrefix = "";
+const basePromptPrefix = "Generate a spreadsheet with following information:";
+
+
+
 const generateAction = async (req, res) => {
   // Run first prompt
-  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+  //console.log(`API: ${basePromptPrefix}${req.body.userInput}${req.body.userInput2}${req.body.userInput3} `)
+
+  const finalPrompt =
+    `
+  ${basePromptPrefix}
+  Title: ${req.body.userInput}
+  Number of Columns : ${req.body.userInput2}
+  Column Names: ${req.body.userInput3}
+  `
 
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    //    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    prompt: `${finalPrompt}\n`,
     temperature: 0.7,
-    max_tokens: 250,
+    max_tokens: 950,
   });
-  
+
   const basePromptOutput = baseCompletion.data.choices.pop();
 
   res.status(200).json({ output: basePromptOutput });
--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,26 @@ import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
-  const [userInput, setUserInput] = useState('');
+  const [userInput, setUserInput] = useState('');    //spreadsheet topic
+  const [userInput2, setUserInput2] = useState('');  // number of columns
+  const [userInput3, setUserInput3] = useState('');  // column names , or | separated
   const [apiOutput, setApiOutput] = useState('')
   const [isGenerating, setIsGenerating] = useState(false)
 
   const callGenerateEndpoint = async () => {
     setIsGenerating(true);
 
-    console.log("Calling OpenAI...")
+    //console.log("Calling OpenAI...")
+    //console.log("TextBoxes", userInput, userInput2, userInput3)  //RJV
+    //console.log(JSON.stringify({ userInput, userInput2, userInput3 })) //RJV
+
+
     const response = await fetch('/api/generate', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ userInput }),
+      body: JSON.stringify({ userInput, userInput2, userInput3 }),
     });
 
     const data = await response.json();
@@ -31,6 +37,14 @@ const Home = () => {
     //console.log(event.target.value);
     setUserInput(event.target.value);
   };
+  const onUserChangedText2 = (event) => {
+    //console.log(event.target.value);
+    setUserInput2(event.target.value);
+  };
+  const onUserChangedText3 = (event) => {
+    //console.log(event.target.value);
+    setUserInput3(event.target.value);
+  };
   return (
     <div className="root">
       <Head>
@@ -39,7 +53,7 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>Generate Spreadsheets about any topic!!!</h1>
+            <h1>Generate Spreadsheets about whatever...!</h1>
           </div>
           <div className="header-subtitle">
             <h2>Just define the subject, number of columns and column headings to get spreadsheet</h2>
@@ -49,10 +63,30 @@ const Home = () => {
         <div className="prompt-container">
           <textarea
             className="prompt-box"
-            placeholder="start typing here"
+            placeholder="Spreadsheet topic/about ...."
             value={userInput}
             onChange={onUserChangedText}
+          />
+          {/* RJV Code */}
+          <div className="normtext">
+            <p> Number of columns in a spreadsheet: (eg 2 or 3 or whatever</p>
+          </div>
+          <textarea
+            className="prompt2-box"
+            placeholder="number of columns"
+            value={userInput2}
+            onChange={onUserChangedText2}
           />;
+          <div className="normtext">
+            <p> Column Names: (separated by pipe character | )</p>
+          </div>
+          <textarea
+            className="prompt-box"
+            placeholder="Column names (separated by pipe character | )"
+            value={userInput3}
+            onChange={onUserChangedText3}
+          />;
+
           {/* New code I added here */}
           <div className="prompt-buttons">
             <a
